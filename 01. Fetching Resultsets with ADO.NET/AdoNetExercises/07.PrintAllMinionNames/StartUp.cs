c6a5d8364df37c30f043b51eaad5ecff520f2aa9
Gandjurov <gandjurov@abv.bg>
2019-02-22 16:00:42 +0200

    add seventh task

---
+++ 01. Fetching Resultsets with ADO.NET/AdoNetExercises/07.PrintAllMinionNames/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace PrintAllMinionNames
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string> names = new List<string>(); 
+
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string sqlQuery = "SELECT Name FROM Minions";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            names.Add((string)reader[0]);
+                        }
+                    }
+                }
+            }
+
+            //for tests:
+            //names = new List<string>();
+            //names.Add("1");
+            //names.Add("2");
+            //names.Add("3");
+            //names.Add("4");
+
+            for (int i = 0; i < names.Count / 2; i++)
+            {
+                Console.WriteLine(names[i]);
+                Console.WriteLine(names[names.Count - 1 - i]);
+            }
+
+            if (names.Count % 2 != 0)
+            {
+                Console.WriteLine(names[names.Count / 2]);
+            }
+        }
+    }
+}
