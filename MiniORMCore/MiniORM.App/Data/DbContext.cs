94edbb21564b111aadc979fdfa7366f7b7cf970f
Gandjurov <gandjurov@abv.bg>
2019-02-25 14:15:16 +0200

    Add few classes in new project

---
+++ MiniORMCore/MiniORM.App/Data/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniORM.App.Data
+{
+    public class DbContext
+    {
+
+    }
+}
---
+++ MiniORMCore/MiniORM.App/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MiniORM.App
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
