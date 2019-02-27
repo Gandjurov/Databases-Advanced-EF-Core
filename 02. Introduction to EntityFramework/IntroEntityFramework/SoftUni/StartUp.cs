5fd713aabe428b8841ae1a7eae43d46d81b2af60
Gandjurov <gandjurov@abv.bg>
2019-02-27 23:24:28 +0200

    Finish with 08. Addresses by Town

--- 02. Introduction to EntityFramework/IntroEntityFramework/SoftUni/StartUp.cs
+++ 02. Introduction to EntityFramework/IntroEntityFramework/SoftUni/StartUp.cs
@@ -13,7 +13,7 @@ namespace SoftUni
         {
             using (var context = new SoftUniContext())
             {
-                var result = GetEmployeesInPeriod(context);
+                var result = GetAddressesByTown(context);
                 Console.WriteLine(result);
             }
         }
@@ -165,5 +165,32 @@ namespace SoftUni
 
             return sb.ToString().Trim();
         }
+
+        //TASK - 08. Addresses by Town
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var addresses = context.Addresses
+                                   .OrderByDescending(a => a.Employees.Count)
+                                   .ThenBy(a => a.Town.Name)
+                                   .Take(10)
+                                   .Select(a => new
+                                   {
+                                       AddressText = a.AddressText,
+                                       Town = a.Town.Name,
+                                       EmployeesCount = a.Employees.Count
+                                   })
+                                   .ToList();
+
+            foreach (var address in addresses)
+            {
+                sb.AppendLine($"{address.AddressText}, {address.Town} - {address.EmployeesCount} employees");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        
     }
 }
