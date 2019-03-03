d53dcbe91ea3823abc3a0cbcb06009696ca596f0
Gandjurov <gandjurov@abv.bg>
2019-03-03 22:33:23 +0200

    add migrations

--- 03.CodeFirst/01.HospitalDatabase/Data/HospitalContext.cs
+++ 03.CodeFirst/01.HospitalDatabase/Data/HospitalContext.cs
@@ -28,109 +28,13 @@ namespace P01_HospitalDatabase.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=HospitalDatabase;Integrated Security=True;");
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString, s => s.MigrationsAssembly("01.HospitalDatabase"));
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Patient>(entity =>
-            {
-                entity.ToTable("Patients");
-
-                entity.HasKey(e => e.PatientId);
-
-                entity.Property(e => e.PatientId).HasColumnName("PatientID");
-
-                entity.Property(e => e.FirstName)
-                    .HasMaxLength(50)
-                    .IsUnicode(true);
-
-                entity.Property(e => e.LastName)
-                    .HasMaxLength(50)
-                    .IsUnicode(true);
-
-                entity.Property(e => e.Address)
-                    .HasMaxLength(250)
-                    .IsUnicode(true);
-
-                entity.Property(e => e.Email)
-                    .HasMaxLength(80)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<Visitation>(entity =>
-            {
-                entity.ToTable("Visitations");
-
-                entity.HasKey(e => e.VisitationId);
-
-                entity.Property(e => e.VisitationId).HasColumnName("VisitationID");
-
-                entity.Property(e => e.Comments)
-                    .HasMaxLength(250)
-                    .IsUnicode(true);
-
-                entity.HasOne(e => e.Patient)
-                    .WithMany(p => p.Visitations)
-                    .HasForeignKey(e => e.PatientId)
-                    .OnDelete(DeleteBehavior.Restrict)
-                    .HasConstraintName("FK_Visitations_Patients");
-            });
-
-            modelBuilder.Entity<Diagnose>(entity =>
-            {
-                entity.ToTable("Diagnoses");
-
-                entity.HasKey(e => e.DiagnoseId);
-
-                entity.Property(e => e.DiagnoseId).HasColumnName("DiagnoseID");
-
-                entity.Property(e => e.Name)
-                    .HasMaxLength(50)
-                    .IsUnicode(true);
-
-                entity.Property(e => e.Comments)
-                    .HasMaxLength(250)
-                    .IsUnicode(true);
-
-                entity.HasOne(e => e.Patient)
-                    .WithMany(p => p.Diagnoses)
-                    .HasForeignKey(e => e.PatientId)
-                    .OnDelete(DeleteBehavior.Restrict)
-                    .HasConstraintName("FK_Diagnoses_Patients");
-            });
-
-            modelBuilder.Entity<Medicament>(entity =>
-            {
-                entity.ToTable("Medicaments");
-
-                entity.HasKey(e => e.MedicamentId);
-
-                entity.Property(e => e.MedicamentId).HasColumnName("MedicamentID");
-
-                entity.Property(e => e.Name)
-                    .HasMaxLength(50)
-                    .IsUnicode(true);
-            });
-
-            modelBuilder.Entity<PatientMedicament>(entity =>
-            {
-                entity.HasKey(e => new { e.PatientId, e.MedicamentId });
-
-                entity.HasOne(e => e.Patient)
-                    .WithMany(p => p.Prescriptions)
-                    .HasForeignKey(e => e.PatientId)
-                    .OnDelete(DeleteBehavior.Restrict)
-                    .HasConstraintName("FK_PatientMedicament_Patients");
-
-                entity.HasOne(e => e.Medicament)
-                    .WithMany(m => m.Presciptions)
-                    .HasForeignKey(e => e.MedicamentId)
-                    .OnDelete(DeleteBehavior.Restrict)
-                    .HasConstraintName("FK_PatientMedicament_Medicaments");
-            });
-
+            
         }
 
     }
--- 03.CodeFirst/01.HospitalDatabase/StartUp.cs
+++ 03.CodeFirst/01.HospitalDatabase/StartUp.cs
@@ -1,4 +1,7 @@
-﻿using P01_HospitalDatabase.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using P01_HospitalDatabase.Data;
 using System;
 
 namespace P01_HospitalDatabase 
@@ -7,10 +10,25 @@ namespace P01_HospitalDatabase
     {
         public static void Main()
         {
-            using (var dbContext = new HospitalContext())
+            //using (var dbContext = new HospitalContext())
+            //{
+            //    DatabaseInitializer.ResetDatabase(dbContext);
+            //}
+
+            LoggerFactory SqlCommandLoggerFactory = new LoggerFactory(new[]
             {
-                DatabaseInitializer.ResetDatabase(dbContext);
-            }
+                new ConsoleLoggerProvider((category, level)
+                    => category == DbLoggerCategory.Database.Command.Name 
+                    && level == LogLevel.Information, true)
+            });
+
+            DbContextOptionsBuilder<HospitalContext> optionBuilder = new DbContextOptionsBuilder<HospitalContext>();
+
+            optionBuilder
+                .UseSqlServer(Configuration.ConnectionString, s => s.MigrationsAssembly("01.HospitalDatabase"))
+                .UseLoggerFactory(SqlCommandLoggerFactory)
+                .EnableSensitiveDataLogging();
+
         }
     }
 }
