56bf100d2973868d25b4efdcb45fca467eb7ab01
Gandjurov <gandjurov@abv.bg>
2019-03-03 22:34:05 +0200

    add Configuration.cs

---
+++ 03.CodeFirst/02.HospitalDatabaseModification/Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=GANDJUROV;Database=Hospital;Integrated Security=True;";
+    }
+}
