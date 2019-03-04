aa61351124e9907f744326dc5f6d3235d93e6b5c
Gandjurov <gandjurov@abv.bg>
2019-03-04 12:39:42 +0200

    Add 03.SalesDatabase

--- 03.CodeFirst/02.HospitalDatabaseModification/StartUp.cs
+++ 03.CodeFirst/02.HospitalDatabaseModification/StartUp.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using P01_HospitalDatabase.Data;
+using System;
 
 namespace P01_HospitalDatabase
 {
@@ -6,7 +10,19 @@ namespace P01_HospitalDatabase
     {
         public static void Main(string[] args)
         {
+            LoggerFactory SqlCommandLoggerFactory = new LoggerFactory(new[]
+            {
+                new ConsoleLoggerProvider((category, level)
+                    => category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information, true)
+            });
 
+            DbContextOptionsBuilder<HospitalContext> optionBuilder = new DbContextOptionsBuilder<HospitalContext>();
+
+            optionBuilder
+                .UseSqlServer(Configuration.ConnectionString, s => s.MigrationsAssembly("03.SalesDatabase"))
+                .UseLoggerFactory(SqlCommandLoggerFactory)
+                .EnableSensitiveDataLogging();
         }
     }
 }
---
+++ 03.CodeFirst/03.SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext() 
+        {
+        }
+
+        public SalesContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString, s => s.MigrationsAssembly("03.SalesDatabase"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
