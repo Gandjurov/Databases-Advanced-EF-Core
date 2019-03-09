7d265cf7dba44694c57505ac8b629cf7dd3c777c
Gandjurov <gandjurov@abv.bg>
2019-03-10 01:53:07 +0200

    add part of first task P01_StudentSystem

---
+++ 04. Entity Relations/P01_StudentSystem/Data/Models/PersonName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class PersonName
+    {
+        public PersonName(string firstName, string lastName)
+        {
+            //if (firstName.Length + lastName.Length > 99)
+            //{
+            //    throw new ArgumentException("Name must be not longer than 100 symbols");
+            //}
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        [Column("FirstName")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Column("LastName")]
+        [Required]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
---
+++ 04. Entity Relations/P01_StudentSystem/Data/StudentDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext()
+        {
+        }
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                        .HasKey(s => s.StudentId);
+
+            modelBuilder.Entity<Student>()
+                        .OwnsOne(s => s.Name);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+    }
+}
