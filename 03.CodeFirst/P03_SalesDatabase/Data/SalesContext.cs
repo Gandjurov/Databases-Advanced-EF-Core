f7b127af1bdfad91258e6712c5836de9ce403084
Gandjurov <gandjurov@abv.bg>
2019-03-09 15:18:37 +0200

    Make whole database with 20190309131632_ProductsAddColumnDescription migration

---
+++ 03.CodeFirst/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,106 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using P03_SalesDatabase.Data.Models;
+
+    public class SalesContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingProduct(modelBuilder);
+
+            OnModelCreatingCustomer(modelBuilder);
+
+            OnModelCreatingStores(modelBuilder);
+
+            OnModelCreatingSales(modelBuilder);
+        }
+
+        private void OnModelCreatingSales(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Sale>()
+                .HasKey(s => s.SaleId);
+        }
+
+        private void OnModelCreatingStores(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Store>()
+                .HasKey(s => s.StoreId);
+
+            modelBuilder
+                .Entity<Store>()
+                .Property(s => s.Name)
+                .HasMaxLength(80)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Store>()
+                .HasMany(s => s.Sales)
+                .WithOne(s => s.Store);
+        }
+
+        private void OnModelCreatingCustomer(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Customer>()
+                .HasKey(c => c.CustomerId);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Email)
+                .HasMaxLength(80);
+
+            modelBuilder
+                .Entity<Customer>()
+                .HasMany(c => c.Sales)
+                .WithOne(s => s.Customer);
+        }
+
+        private static void OnModelCreatingProduct(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Product>()
+                .HasKey(p => p.ProductId);
+
+            modelBuilder
+                .Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Product>()
+                .HasMany(p => p.Sales)
+                .WithOne(s => s.Product);
+
+            modelBuilder
+                .Entity<Product>()
+                .Property(p => p.Description)
+                .HasMaxLength(250);
+        }
+    }
+}
