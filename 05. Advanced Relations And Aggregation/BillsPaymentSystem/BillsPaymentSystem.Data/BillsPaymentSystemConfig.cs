f750b7fbb5ab2c41c9522c0cad7c0060a69a63be
Gandjurov <gandjurov@abv.bg>
2019-03-16 16:18:59 +0200

    Add settings in BillsPaymentSystemConfig.cs

--- 05. Advanced Relations And Aggregation/BillsPaymentSystem/BillsPaymentSystem.Data/BillsPaymentSystemConfig.cs
+++ 05. Advanced Relations And Aggregation/BillsPaymentSystem/BillsPaymentSystem.Data/BillsPaymentSystemConfig.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using BillsPaymentSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BillsPaymentSystem.Data
 {
-    public class BillsPaymentSystemConfig
+    public class BillsPaymentSystemConfig : DbContext
     {
+        public BillsPaymentSystemConfig()
+        {
+        }
+
+        public DbSet<BankAccount> BankAccount { get; set; }
+        public DbSet<CreditCard> CreditCard { get; set; }
+        public DbSet<PaymentMethod> PaymentMethod { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ 05. Advanced Relations And Aggregation/BillsPaymentSystem/BillsPaymentSystem.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillsPaymentSystem.Data
+{
+    internal static class Configuration
+    {
+        internal static string ConnectionString => @"Server=GANDJUROV;Database=BillsPaymentSystem;Integrated Security=True;";
+    }
+}
