dd5f1f52a92e7fa8ed79a2342587464f25679cb3
Gandjurov <gandjurov@abv.bg>
2019-03-22 14:14:08 +0200

    Add ExitCommand.cs

---
+++ 07. Automapper/C# Auto Mapping Objects/MyApp/Core/Commands/ExitCommand.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MyApp.Core.Commands.Contracts;
+using MyApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Core.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        private readonly MyAppContext context;
+        private readonly Mapper mapper;
+
+        public ExitCommand(MyAppContext context, Mapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public string Execute(string[] inputArgs)
+        {
+            Environment.Exit(0);
+            return string.Empty;
+        }
+    }
+}
