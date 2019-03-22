22db128731402ac3866a4f15c40eb1c05072a2f1
Gandjurov <gandjurov@abv.bg>
2019-03-22 12:47:19 +0200

    Add Dependency Injection and Command Pattern

--- 07. Automapper/C# Auto Mapping Objects/MyApp/StartUp.cs
+++ 07. Automapper/C# Auto Mapping Objects/MyApp/StartUp.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Core;
+using MyApp.Core.Contracts;
+using MyApp.Data;
+using System;
 
 namespace MyApp
 {
@@ -6,11 +12,29 @@ namespace MyApp
     {
         public static void Main(string[] args)
         {
+            IServiceProvider services = ConfigureServices();
+            IEngine engine = new Engine(services);
+            engine.Run();
+
             //Database
             //CommandPattern
             //implement Dependancy Injection
             //DTOs
             //Service Layer
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDbContext<MyAppContext>(db => db.UseSqlServer("Server=.;Database=MySpecialApp;Integrated Security=True"));
+
+            serviceCollection.AddTransient<ICommandInterpreter, CommandInterpreter>();
+            serviceCollection.AddTransient<Mapper>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return serviceProvider;
         }
     }
 }
