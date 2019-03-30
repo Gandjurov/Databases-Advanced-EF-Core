547e7d71029732b30ccb151eddb3c72773057c1a
Gandjurov <gandjurov@abv.bg>
2019-03-30 16:22:30 +0200

    10. Import Parts is Done

--- 09. JSON Processing/CarDealerProject/CarDealer/StartUp.cs
+++ 09. JSON Processing/CarDealerProject/CarDealer/StartUp.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using AutoMapper;
 using CarDealer.Data;
 using CarDealer.Models;
+using Microsoft.EntityFrameworkCore.Internal;
 using Newtonsoft.Json;
 
 namespace CarDealer
@@ -25,6 +26,7 @@ namespace CarDealer
             var salesJson = File.ReadAllText(@"..\..\..\Datasets\sales.json");
 
             Console.WriteLine(ImportSuppliers(context, suppliersJson));
+            Console.WriteLine(ImportParts(context, partsJson));
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
@@ -37,6 +39,34 @@ namespace CarDealer
             context.SaveChanges();
 
             return $"Successfully imported {suppliers.Count}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var suppliers = context.Suppliers
+                                   .ToHashSet();
+
+            var parts = JsonConvert.DeserializeObject<List<Part>>(inputJson)
+                           .ToHashSet();
+
+            List<Part> validParts = new List<Part>();
+
+            foreach (var part in parts)
+            {
+                foreach (var supplier in suppliers)
+                {
+                    if (part.SupplierId == supplier.Id)
+                    {
+                        validParts.Add(part);
+                        context.Parts.Add(part);
+                    }
+                }
+            }
+
+
+            context.SaveChanges();
+
+            return $"Successfully imported {validParts.Count}.";
         }
     }
 }
