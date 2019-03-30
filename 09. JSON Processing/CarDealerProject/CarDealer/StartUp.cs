444e01ad1e16df3d6527b743232c2e378c1590bc
Gandjurov <gandjurov@abv.bg>
2019-03-30 14:26:26 +0200

    09. Import Suppliers is Done

--- 09. JSON Processing/CarDealerProject/CarDealer/StartUp.cs
+++ 09. JSON Processing/CarDealerProject/CarDealer/StartUp.cs
@@ -13,6 +13,30 @@ namespace CarDealer
     {
         public static void Main(string[] args)
         {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var suppliersJson = File.ReadAllText(@"..\..\..\Datasets\suppliers.json");
+            var partsJson = File.ReadAllText(@"..\..\..\Datasets\parts.json");
+            var carsJson = File.ReadAllText(@"..\..\..\Datasets\cars.json");
+            var customersJson = File.ReadAllText(@"..\..\..\Datasets\customers.json");
+            var salesJson = File.ReadAllText(@"..\..\..\Datasets\sales.json");
+
+            Console.WriteLine(ImportSuppliers(context, suppliersJson));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson)
+                                       .Where(s => !string.IsNullOrEmpty(s.Name))
+                                       .ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
         }
     }
 }
