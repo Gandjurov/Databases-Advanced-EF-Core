ff087cb98462c4a89246b43e253aa8e269eeb7fd
Gandjurov <gandjurov@abv.bg>
2019-04-02 16:40:32 +0300

    01. Import Users is done

--- 10. XML Processing/ProductShop/ProductShop/StartUp.cs
+++ 10. XML Processing/ProductShop/ProductShop/StartUp.cs
@@ -1,10 +1,54 @@
-﻿namespace ProductShop
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ProductShop
 {
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            Mapper.Initialize(x => 
+            {
+                x.AddProfile<ProductShopProfile>();
+            });
 
+            var usersXml = File.ReadAllText("../../../Datasets/users.xml");
+
+            using (ProductShopContext context = new ProductShopContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var usersResult = ImportUsers(context, usersXml);
+
+                Console.WriteLine(usersResult);
+            }
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportUserDto[]), new XmlRootAttribute("Users"));
+
+            var usersDto = (ImportUserDto[])xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var users = new List<User>();
+
+            foreach (var userDto in usersDto)
+            {
+                var user = Mapper.Map<User>(userDto);
+                users.Add(user);
+            }
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
         }
     }
 }
