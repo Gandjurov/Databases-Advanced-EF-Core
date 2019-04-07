9b74fc7c72e2978bf2a35f4a1d0734fbed26ea4c
Gandjurov <gandjurov@abv.bg>
2019-04-07 12:40:36 +0300

    Add Game and Developer models

--- 11. Exam Preparation/Vapor Store/VaporStore/Data/Models/Game.cs
+++ 11. Exam Preparation/Vapor Store/VaporStore/Data/Models/Game.cs
@@ -1,6 +1,35 @@
 ﻿namespace VaporStore.Data.Models
 {
-	public class Game
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    public class Game
     {
-	}
+        public Game()
+        {
+            //this.Purchases = new List<Purchase>();
+            //this.GameTags = new List<GameTag>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(typeof(decimal), "0.00", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+        
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        
+        public int DeveloperId { get; set; }
+        //public Developer Developer { get; set; }
+
+        public int GenreId { get; set; }
+        //public Genre Genre { get; set; }
+
+        //public ICollection<Purchase> Purchases { get; set; }
+        //public ICollection<GameTag> GameTags { get; set; }
+        
+    }
 }
