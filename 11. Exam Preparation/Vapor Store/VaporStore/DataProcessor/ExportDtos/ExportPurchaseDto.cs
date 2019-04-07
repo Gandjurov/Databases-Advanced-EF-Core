9b612453292336b0137e100521a99bdaaf054612
Gandjurov <gandjurov@abv.bg>
2019-04-07 23:47:54 +0300

    add ExportUserPurchasesByType implementation

---
+++ 11. Exam Preparation/Vapor Store/VaporStore/DataProcessor/ExportDtos/ExportPurchaseDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.ExportDtos
+{
+    [XmlType("Purchase")]
+    public class ExportPurchaseDto
+    {
+        [XmlElement("Card")]
+        public string Card { get; set; }
+
+        [XmlElement("Cvc")]
+        public string CVC { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+
+
+        public ExportGameDto Game { get; set; }
+    }
+}
--- 11. Exam Preparation/Vapor Store/VaporStore/DataProcessor/Serializer.cs
+++ 11. Exam Preparation/Vapor Store/VaporStore/DataProcessor/Serializer.cs
@@ -1,9 +1,16 @@
 ﻿namespace VaporStore.DataProcessor
 {
-	using System;
+    using System;
+    using System.Globalization;
+    using System.IO;
     using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
     using Data;
     using Newtonsoft.Json;
+    using VaporStore.Data.Models.Enums;
+    using VaporStore.DataProcessor.ExportDtos;
 
     public static class Serializer
 	{
@@ -25,23 +32,69 @@
                                                  Tags = string.Join(", ", g.GameTags.Select(t => t.Tag.Name).ToArray()),
                                                  Players = g.Purchases.Count
                                              })
-                                            .OrderByDescending(p => p.Players)
-                                            .ThenBy(g => g.Id)
-                                            .ToArray(),
-                                            TotalPlayers = x.Games.Sum(p => p.Purchases.Count)
-                                        })
-                                        .OrderByDescending(t => t.TotalPlayers)
-                                        .ThenBy(g => g.Id)
-                                        .ToArray();
+                                   .OrderByDescending(p => p.Players)
+                                   .ThenBy(g => g.Id)
+                                   .ToArray(),
+                                   TotalPlayers = x.Games.Sum(p => p.Purchases.Count)
+                                })
+                                .OrderByDescending(t => t.TotalPlayers)
+                                .ThenBy(g => g.Id)
+                                .ToArray();
 
-            var jsonResult = JsonConvert.SerializeObject(genres, Formatting.Indented);
+            var jsonResult = JsonConvert.SerializeObject(genres, Newtonsoft.Json.Formatting.Indented);
 
             return jsonResult;
 		}
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
 		{
-			throw new NotImplementedException();
-		}
+            var purchaseType = Enum.Parse<PurchaseType>(storeType);
+
+            var users = context
+                .Users
+                //.Where(p => p.Cards.SelectMany(x => x.Purchases).Any(t => t.Type == purchaseType))
+                .Select(x => new ExportUserDto
+                {
+                    Username = x.Username,
+                    Purchases = x.Cards
+                                 .SelectMany(p => p.Purchases)
+                                 .Where(t => t.Type == purchaseType)
+                                 .Select(p => new ExportPurchaseDto
+                                 {
+                                     Card = p.Card.Number,
+                                     CVC = p.Card.Cvc,
+                                     Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                                     Game = new ExportGameDto
+                                     {
+                                         Genre = p.Game.Genre.Name,
+                                         Title = p.Game.Name,
+                                         Price = p.Game.Price
+                                     }
+                                 })
+                    .OrderBy(d => d.Date)
+                    .ToArray(),
+                    TotalSpent = x.Cards
+                                  .SelectMany(p => p.Purchases)
+                                  .Where(t => t.Type == purchaseType)
+                                  .Sum(p => p.Game.Price)
+                })
+                .Where(p => p.Purchases.Any())
+                .OrderByDescending(t => t.TotalSpent)
+                .ThenBy(u => u.Username)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportUserDto[]), new XmlRootAttribute("Users"));
+            var sb = new StringBuilder();
+            var namespaces = new XmlSerializerNamespaces(new[]
+            {
+                XmlQualifiedName.Empty
+            });
+
+            xmlSerializer.Serialize(new StringWriter(sb), users, namespaces);
+
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+        }
 	}
 }
