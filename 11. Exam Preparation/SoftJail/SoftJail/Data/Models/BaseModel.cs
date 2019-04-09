44d9ba03b1706c21bff6e36fbce4fee921c73ae1
Gandjurov <gandjurov@abv.bg>
2019-04-09 14:55:46 +0300

    add Prisoner model

---
+++ 11. Exam Preparation/SoftJail/SoftJail/Data/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.Data.Models
+{
+    public abstract class BaseModel<T>
+    {
+        [Key]
+        public T Id { get; set; }
+    }
+}
