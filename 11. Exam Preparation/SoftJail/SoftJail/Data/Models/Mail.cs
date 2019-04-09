b4c458ab97c66e1a6354d341f7a54b381212db0b
Gandjurov <gandjurov@abv.bg>
2019-04-09 15:29:39 +0300

    Add Mail model

---
+++ 11. Exam Preparation/SoftJail/SoftJail/Data/Models/Mail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.Data.Models
+{
+    public class Mail : BaseModel<int>
+    {
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Sender { get; set; }
+
+        [RegularExpression(@"^[A-Za-z0-9\s]+ str.$")]
+        public string Address { get; set; }
+
+        public int PrisonerId { get; set; }
+        public Prisoner Prisoner { get; set; }
+    }
+}
