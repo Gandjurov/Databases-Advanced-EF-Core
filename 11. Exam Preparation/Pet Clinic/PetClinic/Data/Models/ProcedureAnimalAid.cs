bc67427109323c07d8aeccfffee965c658c41d88
Gandjurov <gandjurov@abv.bg>
2019-04-11 15:34:43 +0300

    add mapping model ProcedureAnimalAid

---
+++ 11. Exam Preparation/Pet Clinic/PetClinic/Data/Models/ProcedureAnimalAid.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetClinic.Data.Models
+{
+    public class ProcedureAnimalAid
+    {
+        [Required]
+        public int ProcedureId { get; set; }
+        public Procedure Procedure { get; set; }
+
+        [Required]
+        public int AnimalAidId { get; set; }
+        public AnimalAid AnimalAid { get; set; }
+    }
+}
