06d2a297507ccb25f5e1ba4893ce233bfe040837
Gandjurov <gandjurov@abv.bg>
2019-04-11 15:05:49 +0300

    Add Passport Model

---
+++ 11. Exam Preparation/Pet Clinic/PetClinic/Data/Models/Passport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetClinic.Data.Models
+{
+    public class Passport
+    {
+        [Key]
+        [RegularExpression(@"^[A-Za-z]{7}[0-9]{3}$")]
+        public string SerialNumber { get; set; }
+
+        [Required]
+        public Animal Animal { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(\+359|0)[0-9]{9}$")]
+        public int OwnerPhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string OwnerName { get; set; }
+
+        [Required]
+        public DateTime RegistrationDate { get; set; }
+    }
+}
